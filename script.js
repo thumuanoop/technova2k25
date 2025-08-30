d2e8bd621e3c2e7bd698870228bc7b473e7cc3e5
thumuanoop <thumuanoop@gmail.com>
2025-08-30 11:06:18 +0530

    Update script.js

--- script.js
+++ script.js
@@ -126,3 +126,7 @@ function redirectToForm() {
 document.querySelector('.menu-toggle').addEventListener('click', () => {
   document.querySelector('.nav-links').classList.toggle('active');
 });
+function toggleMenu() {
+    const navLinks = document.querySelector('.nav-links');
+    navLinks.classList.toggle('active');
+}
